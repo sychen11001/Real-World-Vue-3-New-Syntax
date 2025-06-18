7dffe345ca1909f21c7378b3c10e7fea357fdd4d
sychen11001 <sychen11001@gmail.com>
2025-06-19 00:56:47 +0800

    编写练习

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import EventListView from '../views/EventListView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'event-list',
+      component: EventListView,
     },
     {
       path: '/about',
@@ -15,9 +15,20 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/event/:id',
+      name: 'event-details',
+      props: true, // 允许将路由参数作为 props 传递给组件
+      component: () => import('../views/EventDetailsView.vue'),
+    },
+    // {
+    //   path: '/event/123',
+    //   name: 'event-details',
+    //   component: () => import('../views/EventDetailsView.vue'),
+    // },
+  ],
 })
 
 export default router
